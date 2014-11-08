61385a602b3922076fb0705e85c97ecd23f04e92
Sage Weil <sage@redhat.com>
2014-11-07 16:35:41 -0800

    osd/Watch: drop redundant in_progress_watchers
    This is essentially watchers.size() at all times and obfuscates the code.
    In particular this makes the maybe_complete_notify() logic much clearer.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -5418,7 +5418,6 @@ void ReplicatedPG::do_osd_op_effects(OpContext *ctx)
       Notify::makeNotifyRef(
 	conn,
 	ctx->reqid.name.num(),
-	ctx->obc->watchers.size(),
 	p->bl,
 	p->timeout,
 	p->cookie,
